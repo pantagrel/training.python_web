5557e6eb4fe57e71905123dba7be394387b564e6
pantagrel <kristin.gannon@gmail.com>
2013-02-03 13:21:10 -0800

    the slog of updates continue

--- assignments/week04/athome/week04_bookDB.py
+++ assignments/week04/athome/week04_bookDB.py
@@ -5,21 +5,18 @@ from cgi import escape, parse_qs
 import bookdb
 from wsgiref.simple_server import make_server
 
-# map urls to functions
-urls = []
-
 books = bookdb.BookDB()
 
 # one function lists all books
 def index(environ, start_response):
 	"""this page shows the list of books as links"""
+	links = ''
 	for entry in books.titles():
 # 		make a link with book title
-		link = ''
-		link += '<p><a href="#">' + str(entry) + '</p>'
+		links += '<p><a href="#">' + str(entry['title']) + '</p>'
 	start_response('200 OK', [('Content-Type', 'text/html')])
-	header = "<html><head><title>Kristin Gannon | Book Database</title></head>"
-	body = "<body><h1>Book Database:</h1>%s</body></html>" % link
+	header = "<html><head><title>Kristin Gannon | Book Database Thingamajig</title></head>"
+	body = "<body><h1>Book Database:</h1>%s</body></html>" % links
 	return [header + body]
 
 
@@ -32,6 +29,13 @@ def not_found(environ, start_response):
     start_response('404 NOT FOUND', [('Content-Type', 'text/plain')])
     return ['Not Found']
     
+# map urls to functions
+urls = [
+    (r'^$', index),
+    (r'hello/?$', index),
+    (r'hello/(.+)$', index)   
+]
+
     
 def application(environ, start_response):
     """
@@ -51,6 +55,6 @@ def application(environ, start_response):
     return not_found(environ, start_response)
 		
 if __name__ == '__main__':
-	srv = make_server('localhost', 8080, index)
+	srv = make_server('localhost', 8080, application)
 	srv.serve_forever()
 
